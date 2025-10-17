7f2c53a95a3505739acf4557c9f6773514245e30
Kalle Värna <25kava@stockholmscience.se>
2025-10-17 22:37:01 +0200

    Pathfinder v2

--- pathfinder/maze.py
+++ pathfinder/maze.py
@@ -4,24 +4,20 @@ import sys
 import time
 import pygame
 
-pygame.init()
 
 maze = []
-goalCords = [39, 39]
-startCords = [1, 1]
 
 window = pygame.display.set_mode((820, 820))
 tileSize = 20
 
-Black = (0,0,0)
-White = (220,220,220)
-Blue = (50,150,255)
-Red = (255,80,80)
-Grey = (120,120,120)
+Black = (0, 0, 0)
+White = (220, 220, 220)
+Blue = (50, 150, 255)
+Red = (255, 80, 80)
+Grey = (120, 120, 120)
 
 
-
-with open("maze.txt", "r") as f:
+with open(r"c:\Users\25kava\Desktop\VSCode\Python\pathfinder\maze.txt", "r") as f:
     for line in f:
         row = []
         line = line.strip()
@@ -33,10 +29,14 @@ with open("maze.txt", "r") as f:
             elif char == "A":
                 row.append(2)
             elif char == "B":
-                row.append(3)     
+                row.append(3)
         maze.append(row)
 
-pathfinder = Player(startCords)
+goalCords = [39, 39]
+startCords = [1, 1]
+
+pathfinder = PlayerV2(startCords, goalCords)
+
 
 def drawMap():
     for row_idx, row in enumerate(maze):
@@ -51,32 +51,39 @@ def drawMap():
                 color = Red
             elif cell == 4:
                 color = Grey
-            
-            pygame.draw.rect(window, color, (col_idx*tileSize, row_idx*tileSize, tileSize, tileSize))
-        
-        pygame.draw.rect(window, finderCol, (finderPos[0]*tileSize, finderPos[1]*tileSize, tileSize, tileSize))
+
+            pygame.draw.rect(window, color, (col_idx*tileSize,
+                             row_idx*tileSize, tileSize, tileSize))
+
+    pygame.draw.rect(
+        window, finderCol, (finderPos[0]*tileSize, finderPos[1]*tileSize, tileSize, tileSize))
 
     pygame.display.update()
-        
 
 
+finderPos = None
+
+pygame.init()
+t = time.time()
 running = True
+
 while running:
-    while pathfinder.isDeadEnd(maze):
-        print("Dead end!")
-        finderPos = pathfinder.backtrack()
-        print(pathfinder.steps)
-        sys.exit()
-    
-    finderPos = pathfinder.nextStep(maze)
-    maze[finderPos[1]][finderPos[0]] = 4
-    finderCol = Red
-    
-    drawMap()
+    while finderPos != [39, 39]:
+        pathfinder.filterWalls(maze)
+        pathfinder.notVisited(maze)
+        nextStep = pathfinder.decideNextStep()
+        finderPos = pathfinder.takeNextStep(maze)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        maze[finderPos[1]][finderPos[0]] = 4
+        finderCol = Red
+        drawMap()
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    running = False
+
+print(f"Exit found in {time.time() - t:.2f} seconds!")
 pygame.quit()
-sys.exit("Cya")
+sys.exit()
--- pathfinder/player.py
+++ pathfinder/player.py
@@ -1,13 +1,17 @@
 import random
+import time
+import math
 
-class Player:
-    def __init__(self, pos: list):
+
+class PlayerV2:
+    def __init__(self, pos: list, goalCords: list):
         self.pos = pos
+        self.viableStep = []
         self.steps = []
+        self.end = goalCords
 
-    def isDeadEnd(self, map: list) -> bool:
-        cases = 0
-        und = 0
+    def filterWalls(self, map: list[int, int]) -> list[str]:
+        self.viableStep = ['up', 'down', 'left', 'right']
         x, y = self.pos
 
         up = map[y-1][x] != 1
@@ -15,68 +19,100 @@ class Player:
         left = map[y][x-1] != 1
         right = map[y][x+1] != 1
 
-        if up != True: cases +=1
-        if down != True: cases +=1
-        if left != True: cases +=1
-        if right != True: cases +=1
-        
-        if map[y-1][x] != 4: und +=1
-        if map[y+1][x] != 4: und +=1
-        if map[y][x-1] != 4: und +=1
-        if map[y][x+1] != 4: und+=1
+        if not up:
+            self.viableStep.remove('up')
+        if not down:
+            self.viableStep.remove('down')
+        if not left:
+            self.viableStep.remove('left')
+        if not right:
+            self.viableStep.remove('right')
 
-        if cases >= 3 and self.pos != [1,1 or [39,39]]:
-            while und == 0:
-                return True
-        else:
-            return False
-        
-    def nextStep(self, map: list) -> list:
-        print("pos:", self.pos, "maze size:", len(map), len(map[0]))
+        return self.viableStep
 
+    def notVisited(self, map: list[int, int]) -> list[str]:
         x, y = self.pos
-        self.steps.append(self.pos)
-
-        up = map[y-1][x] != 1
-        down = map[y+1][x] != 1
-        left = map[y][x-1] != 1
-        right = map[y][x+1] != 1
+        temp = self.viableStep.copy()
 
         _up = map[y-1][x] != 4
         _down = map[y+1][x] != 4
         _left = map[y][x-1] != 4
         _right = map[y][x+1] != 4
-        
 
-        print(f"{up} | {down} | {left} | {right}")
-        print(f"{_up} | {_down} | {_left} | {_right}")
+        if not _up:
+            self.viableStep.remove('up')
+        if not _down:
+            self.viableStep.remove('down')
+        if not _left:
+            self.viableStep.remove('left')
+        if not _right:
+            self.viableStep.remove('right')
 
-        moved = False
+        if len(self.viableStep) == 0:
+            return self.viableStep
+        else:
+            return self.viableStep
 
-        dirs = ['up', 'down', 'left', 'right']
-        dir = dirs[random.randint(0, 3)]
+    def decideNextStep(self) -> None:
+        if len(self.viableStep) > 1:
+            self.nextStep = self.decideBias(self.viableStep)
+        elif len(self.viableStep) == 1:
+            self.nextStep = self.viableStep[0]
+        else:
+            self.nextStep = 'BT'
 
-        if _up and up:
-            self.pos = [x, y-1]; moved = True
-        elif _down and down:
-            self.pos = [x, y+1]; moved = True
-        elif _left and left:
-            self.pos = [x-1, y]; moved = True
-        elif _right and right:
-            self.pos = [x+1, y]; moved = True
+    def calcHypo(self, x1: int, x2: int, y1: int, y2: int):
+        return math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
 
-        if not moved:
-            if up and dir:
-                self.pos = [x, y-1]; moved = True
-            elif down and dir:
-                self.pos = [x, y+1]; moved = True
-            elif left:
-                self.pos = [x-1, y]; moved = True
-            elif right:
-                self.pos = [x+1, y]; moved = True
+    def decideBias(self, steps: list[str]) -> str:
+        x, y = self.pos
+        endX, endY = self.end
 
-        print(self.pos)
+        smallest = 10e10
+        smallestIndex = None
+
+        for index, candidate in enumerate(steps):
+            if candidate is "up":
+                temp = self.calcHypo(endX, x, endY, y-1)
+            if candidate is "down":
+                temp = self.calcHypo(endX, x, endY, y+1)
+            if candidate is "left":
+                temp = self.calcHypo(endX, x-1, endY, y)
+            if candidate is "right":
+                temp = self.calcHypo(endX, x+1, endY, y)
+
+            if temp < smallest:
+                smallest = temp
+                smallestIndex = index
+
+        return steps[smallestIndex]
+
+    def takeNextStep(self, map: list) -> list:
+        x, y = self.pos
+
+        if self.nextStep == 'up':
+            self.pos = [x, y-1]
+        elif self.nextStep == 'down':
+            self.pos = [x, y+1]
+        elif self.nextStep == 'left':
+            self.pos = [x-1, y]
+        elif self.nextStep == 'right':
+            self.pos = [x+1, y]
+        else:
+            self.pos = self.backtrack(map)
+
+        if self.nextStep != 'BT':
+            self.steps.append(self.pos)
+
         return self.pos
 
-    def steps(self) -> list:
-        return self.steps
+    def backtrack(self, map: list) -> list:
+
+        self.pos = self.steps.pop()
+        self.filterWalls(map)
+        possible = self.notVisited(map)
+
+        if possible:
+            return self.pos
+
+        return self.pos
